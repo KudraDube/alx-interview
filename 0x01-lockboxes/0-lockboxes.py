da5657369be4ac0e3fceabde7b52d396c8603711
kudradube <kudradube@gmail.com>
2023-07-07 11:33:07 +0000

    update to 0x04 Tasks

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,32 @@
 #!/usr/bin/python3
-'''task's 0 Module
-'''
+'''LockBoxes Challenge'''
 
 
 def canUnlockAll(boxes):
-    key = [0]
-    for k in key:
-        for i in boxes[k]:
-            if i not in key and i < len(boxes):
-                key.append(i)
-    if len(key) == len(boxes):
-        return True
-    else:
-        return False
+    '''determines if all the boxes can be opened or not
+    Returns:
+        True: all boxes can be opened
+        False: not all boxes can be opened
+    '''
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
 
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
+            continue
+        else:
+            break
+
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
