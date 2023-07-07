209a092c4d91a3274d1aa67db88d3fdfe6777203
kudradube <kudradube@gmail.com>
2023-07-07 11:32:26 +0000

    update to 0x04 Tasks

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-"""
-Pascal triangle module
-"""
-#This better be it
+"""Pascal Triangle Interview Challenge"""
+
 
 def pascal_triangle(n):
-    """ returns the Pascal's Triangle of size n. """
+    """returns a list of lists of numbers
+    representing the pascal triangle"""
     if n <= 0:
         return []
 
-    pascalTriangle = [[1]]
-    while len(pascalTriangle) != n:
-        last_row = pascalTriangle[-1]
-        next_row = [1]
+    pascal_triangle = [0] * n
 
-        for i in range(len(last_row) - 1):
-            next_row.append(last_row[i] + last_row[i + 1])
-        next_row.append(1)
-        pascalTriangle.append(next_row)
+    for i in range(n):
+        # define a row and fill first and last idx with 1
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
 
-    return pascalTriangle
+        for j in range(1, i):
+            if j > 0 and j < len(new_row):
+                a = pascal_triangle[i - 1][j]
+                b = pascal_triangle[i - 1][j - 1]
+                new_row[j] = a + b
+
+        pascal_triangle[i] = new_row
+
+    return pascal_triangle
