b63d5e0479c7b97bc81aa7ff1504c6eccebba48b
kudradube <kudradube@gmail.com>
2023-07-07 11:03:18 +0000

    update to 0x04 Tasks

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-'''
-Returns list representing the Pascal's triangle of n
-'''
-
+"""
+Pascal triangle module
+"""
+#This better be it
 
 def pascal_triangle(n):
-    '''returns empty list if n <= 0'''
+    """ returns the Pascal's Triangle of size n. """
     if n <= 0:
         return []
 
-    triangle = []
-    row = []
-    prev_row = []
-    for i in range(0, n + 1):
-        row = [j > 0 and j < i - 1 and i > 2 and prev_row[j - 1] +
-               prev_row[j] or 1 for j in range(0, i)]
-        prev_row = row
-        triangle += [row]
-    return triangle[1:]
+    pascalTriangle = [[1]]
+    while len(pascalTriangle) != n:
+        last_row = pascalTriangle[-1]
+        next_row = [1]
 
+        for i in range(len(last_row) - 1):
+            next_row.append(last_row[i] + last_row[i + 1])
+        next_row.append(1)
+        pascalTriangle.append(next_row)
+
+    return pascalTriangle
