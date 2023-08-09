33c14b3917a2ab9827cf71f4714010f4ff9e1d22
kudradube <kudradube@gmail.com>
2023-08-09 12:06:36 +0000

    0x06 ALX Task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,21 +5,21 @@ const request = require("request");
 const movieNum = process.argv[2];
 const URL = `https://swapi-api.alx-tools.com/api/films/${movieNum}`;
 
-// Makes API request, sets async to allow await promise
+// Kudra Makes API request, sets async to allow await promise Kudra
 request(URL, async function (err, res, body) {
   if (err) return console.error(err);
 
-  // parse each character in the film as a list objects
+  // parse each character in the film as a list objects Remember this Kudra
   const charURLList = JSON.parse(body).characters;
 
-  // Use URL list to character pages to make new requests
-  // await queues requests until they resolve in order
+  // Use URL list to character pages to make new requests Dra man
+  // await queues requests until they resolve in order DraKu
   for (const charURL of charURLList) {
     await new Promise(function (resolve, reject) {
       request(charURL, function (err, res, body) {
         if (err) return console.error(err);
 
-        // finds each character name and prints in URL order
+        // finds each character name and prints in URL order DraKu Dube
         console.log(JSON.parse(body).name);
         resolve();
       });
