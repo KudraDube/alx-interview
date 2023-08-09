68495826b4e029836d3b308d7ca7aa3e38abd673
kudradube <kudradube@gmail.com>
2023-08-09 12:16:11 +0000

    0x06 ALX Task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,29 +1,17 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 
-const movieNum = process.argv[2];
-const URL = `https://swapi-api.alx-tools.com/api/films/${movieNum}`;
-
-// Kudra Makes API request, sets async to allow await promise Kudra
-request(URL, async function (err, res, body) {
-  if (err) return console.error(err);
-
-  // parse each character in the film as a list objects Remember this Kudra
-  const charURLList = JSON.parse(body).characters;
-
-  // Use URL list to character pages to make new requests Dra man
-  // await queues requests until they resolve in order DraKu
-  for (const charURL of charURLList) {
-    await new Promise(function (resolve, reject) {
-      request(charURL, function (err, res, body) {
-        if (err) return console.error(err);
-
-        // finds each character name and prints in URL order DraKu Dube
-        console.log(JSON.parse(body).name);
-        resolve();
-      });
-    });
-  }
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
-
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
